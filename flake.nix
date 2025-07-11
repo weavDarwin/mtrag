fc29428d2a12e1d90b21067b940f36d9d4c59db0
Darwin Corn <darwin@weaviate.io>
2025-07-11 04:26:06 -0700

    Demo draft

--- flake.nix
+++ flake.nix
@@ -28,6 +28,19 @@
         devShells.default = pkgs.mkShell {
           inherit nativeBuildInputs buildInputs;
           name = "mtrag";
+          shellHook = ''
+            if [ ! -f "AllPrintings.sqlite" ]; then
+              echo "Downloading AllPrintings.sqlite.gz from https://mtgjson.com/api/v5/AllPrintings.sqlite.gz"
+              curl -O https://mtgjson.com/api/v5/AllPrintings.sqlite.gz
+              echo "AllPrintings.sqlite.gz downloaded"
+              echo "Unzipping AllPrintings.sqlite.gz"
+              gunzip AllPrintings.sqlite.gz
+              echo "AllPrintings.sqlite.gz unzipped"
+            else
+              echo "AllPrintings.sqlite already exists, skipping download"
+            fi
+            echo "Welcome to the mtrag development environment!"
+          '';
         };
 
         packages.default = pkgs.buildGoModule rec {
